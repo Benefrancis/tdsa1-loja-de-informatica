2916bff68db0d9f2ad6ef64ef3e3bb754e2d34bc
Benefrancis <benefrancis@gmail.com>
2023-04-03 00:30:08 -0300

    dIAGRAMAS

--- src/main/java/br/com/benefrancis/dominio/computador/Computador.java
+++ src/main/java/br/com/benefrancis/dominio/computador/Computador.java
@@ -4,13 +4,16 @@ import br.com.benefrancis.dominio.computador.abstracoes.Componente;
 import com.google.gson.Gson;
 
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Computador {
-    Map<String, Componente> componentes = new TreeMap();
+    Map<String, Componente> componentes;
 
     public Computador(Map<String, Componente> componentes) {
         this.componentes = componentes;
+    }
+
+    public Map<String, Componente> getComponentes() {
+        return componentes;
     }
 
     @Override
